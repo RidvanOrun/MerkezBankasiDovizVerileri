9727260624c9629455d4e633b7f90123ef482f32
Rıdvan ÖRÜN <ridvanorun@outlook.com>
2021-03-27 00:38:58 +0300

    Ön yüz oluşturuldu

--- MerkezBankasıDövizVerileri.Data/Entitiy/Entities.cs
+++ MerkezBankasıDövizVerileri.Data/Entitiy/Entities.cs
@@ -15,10 +15,10 @@ namespace MerkezBankasıDövizVerileri.Data.Entitiy
         public string AUD { get; set; }
         public string EUR { get; set; }
         public string GBP { get; set; }
-
-        private DateTime _createDate;
-        public DateTime CreateDate { get => _createDate; }
         public string CurrencyDate { get; set; }
+
+        private DateTime _createDate=DateTime.Now;
+        public DateTime CreateDate { get => _createDate; set => value = _createDate; }
 
     }
 }
---
+++ MerkezBankasıDövizVerileri.Data/Migrations/20210326210944_Dvz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MerkezBankasıDövizVerileri.Data.Migrations
+{
+    public partial class Dvz : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    USD = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AUD = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EUR = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GBP = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrencyDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entities");
+        }
+    }
+}
